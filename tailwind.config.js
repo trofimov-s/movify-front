d93598e87c80efc0e0bfc0a438232c0fa604df67
sergei-trofimov <feast.st@gmail.com>
2023-07-14 15:12:31 +0300

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  darkMode: "class",
+  theme: {
+    colors: {
+      white: "#ffffff",
+    },
+    fontFamily: {
+      main: "Comfortaa",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
