e21703f623933bc40dac908517cc1a96ccf94f33
Serhii Trofimov <trofim.serhii@gmail.com>
2023-07-22 01:19:12 +0300

    add store

--- src/app/core/store/reducer.ts
+++ src/app/core/store/reducer.ts
@@ -13,5 +13,8 @@ export const homeReducer = createReducer(
   homeState,
   on(HomePageAction.increase, (state) => ({ ...state, counter: state.counter + 1 })),
   on(HomePageAction.decrease, (state) => ({ ...state, counter: state.counter - 1 })),
-  on(HomePageAction.increase_by, (state, { count }) => ({ ...state, counter: state.counter + count }))
+  on(HomePageAction.increase_by, (state, { count }) => ({
+    ...state,
+    counter: state.counter + count,
+  }))
 );
